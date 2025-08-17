de82980b7a4955ecf5edea0df8cefd53ef4bbd7c
Moe Hamade <mohamadhamade30@gmail.com>
2025-08-17 18:50:53 +0300

    Add solution: Length of Longest Substring with One Flip

---
+++ arrays_and_strings/sliding_window/binary_string.cpp
@@ -0,0 +1,38 @@
+/*
+Example 2: You are given a binary string s (a string containing only "0" and "1").
+You may choose up to one "0" and flip it to a "1".
+What is the length of the longest substring achievable that contains only "1"?
+For example, given s = "1101100111", the answer is 5.
+If you perform the flip at index 2, the string becomes 1111100111.
+*/
+
+#include <iostream>
+using namespace std;
+
+int findLength(string s)
+{
+    int left = 0, curr = 0, ans = 0;
+    for (int right = 0; right < s.length(); right++)
+    {
+        if (s[right] == '0')
+        {
+            curr++;
+        }
+        while (curr > 1)
+        {
+            if (s[left] == '0')
+            {
+                curr--;
+            }
+            left++;
+        }
+        ans = max(ans, right - left + 1);
+    }
+    return ans;
+}
+
+int main()
+{
+    string s = "1101100111";
+    cout << findLength(s);
+}
